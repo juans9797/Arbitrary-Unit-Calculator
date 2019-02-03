33db1def5351546cc4d00432a23741a0e40dda0a
Juan Sebastian Rivera <juansebastianrivera@hotmail.com>
2019-02-03 17:07:37 -0500

    Add project files.

---
+++ ABU/ABU/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ABU
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BSettings.Pressed += BSettings_Pressed;
+        }
+
+        private void BSettings_Pressed(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Page1());
+        }
+    }
+}
