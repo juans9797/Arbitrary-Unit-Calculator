995afde8dc92b6297916cf4a42e456b34afca84f
Juan Sebastian Rivera <juansebastianrivera@hotmail.com>
2019-02-03 19:32:20 -0500

    Added changes over settings and maing page

--- ABU/ABU/MainPage.xaml.cs
+++ ABU/ABU/MainPage.xaml.cs
@@ -7,13 +7,20 @@ namespace ABU
     {
         public MainPage()
         {
+            int dCycle = 0;
             InitializeComponent();
             BSettings.Pressed += BSettings_Pressed;
             DateTime Today = DateTime.Now.ToLocalTime();
             DateTime dateOnly = Today.Date;
             CurDate.Text = dateOnly.ToString("d");
+            try
+            {
+               dCycle = Convert.ToInt32(Application.Current.Properties["cycle"].ToString());
+                numCy.Text = "Cycle ("+dCycle.ToString()+" Days):";
+            }
+            catch (Exception e) { }
             var picker = DaysList;
-            for (int i = 0; i < 7;i++)
+            for (int i = 0; i < dCycle;i++)
             {
                 picker.Items.Add(dateOnly.AddDays(-i).ToString("d"));
             }
--- ABU/ABU/Page1.xaml.cs
+++ ABU/ABU/Page1.xaml.cs
@@ -1,20 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ABU
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Page1 : ContentPage
 	{
 		public Page1 ()
 		{
-			InitializeComponent ();
-		}
-	}
+            InitializeComponent ();
+            DaysC.Completed += DaysC_Completed;
+            try
+            {
+                DaysC.Text = Application.Current.Properties["cycle"].ToString();
+            }
+            catch (Exception e){ }
+
+        }
+
+        private void DaysC_Completed(object sender, EventArgs e)
+        {
+            Application.Current.Properties["cycle"] = DaysC.Text;
+            Navigation.PopModalAsync();
+            Navigation.PushModalAsync(new MainPage());
+
+        }
+    }
 }
